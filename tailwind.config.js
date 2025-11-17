ed8d41bef445d07200991bb5e757929c382f104c
Asmaa Abubakr <asmaaabubakr04@gmail.com>
2025-11-17 23:21:55 +0200

    Update: added Nuba trip section and fixed profile background8

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ export default {
     extend: {
        colors: {
         orange: {
-          DEFAULT: '#E9A914',
-          500: '#E9A914',
-          600: '#C58A12', // ظِللْة أغمق لـ hover / active
+          300: '#F8EBC2', // أخف
+          400: '#F1D98A',
+          DEFAULT: '#E7BC58', // ذهبى أخف (أساس)
+          500: '#E7BC58',
+          600: '#C8A255', // أغمق للـ hover/active
+          700: '#9A7A3F', // ظلال أعمق إضافية
         },
       },
     },
